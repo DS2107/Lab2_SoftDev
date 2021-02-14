e536c8304513e8e8935379c56944e339538e3fa3
Danil Struchenckov <danilstru20002203@gmail.com>
2021-02-14 13:17:25 +0300

    Добавьте файлы проекта.

---
+++ Lab2_Stru_IVT-7_V8/FileService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Stru_IVT_7_V8
+{
+    public class FileService
+    {
+        public int MergeTemporaryFiles(string dir)
+        {
+            return 0;
+
+        } // MergeTemporaryFiles
+    }
+}
