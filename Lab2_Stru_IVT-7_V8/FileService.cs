9f3905b9e4d88e5361633d6d21dfa3507ddb4175
Danil Struchenckov <danilstru20002203@gmail.com>
2021-02-14 14:39:55 +0300

    Основное задние(Production Code)

--- Lab2_Stru_IVT-7_V8/FileService.cs
+++ Lab2_Stru_IVT-7_V8/FileService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,41 @@ namespace Lab2_Stru_IVT_7_V8
 {
     public class FileService
     {
+        public static void Main(string[] args)
+        {
+            FileService file = new FileService();
+            _ = file.MergeTemporaryFiles("s");
+        }
         public int MergeTemporaryFiles(string dir)
         {
-            return 0;
+            dir = "tmpFile";
+            if (CheckPath(dir))
+            {
+                var LisFilesTmp = Directory.GetFiles(dir, "*.tmp").ToList();
+                string backupString = "";
+                int count_tmpFiles = 0;
+                foreach (var file in LisFilesTmp)
+                {
+                    backupString = backupString + " " + Path.GetFileName(file);
+                    File.Delete(file);
+                    count_tmpFiles++;
+                }
+                File.WriteAllText("backupFile/backup.tmp", backupString);
 
+                return count_tmpFiles;
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("Не существующий каталог");
+            }
         } // MergeTemporaryFiles
+
+        private bool CheckPath(string dir)
+        {
+            if (Directory.Exists(dir))
+                return true;
+            else
+                return false;
+        }
     }
 }
---
+++ Lab2_Stru_IVT-7_V8/ReportViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Stru_IVT_7_V8
+{
+    class ReportViewer
+    {
+        // свойство которому предается значение - кол-ва файлов .tmp
+        private int BlockCount{get;set;}
+        public void PrepareData()
+        {
+            FileService file = new FileService();
+            BlockCount =  file.MergeTemporaryFiles("tmpFile");
+
+        }
+    }
+}
